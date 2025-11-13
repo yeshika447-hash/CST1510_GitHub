0a52d3d66bbf505a357e9e0742b8897dd5dce22d
yashwinbhautoo <87039854+yashwinbhautoo@users.noreply.github.com>
2025-11-13 14:54:22 +0400

    Week 8 part 1

---
+++ CW2_M01068100_CST1510/App/Data/db.py
@@ -0,0 +1,10 @@
+import sqlite3
+from pathlib import Path
+
+DB_PATH =Path("DATA") / "intelligence_platform.db"
+
+def connect_database(db_path=DB_PATH):
+    db_path.parent.mkdir(parents=True, exist_ok=True)
+    connection = sqlite3.connect(str(db_path))
+    print(f"Connected to database at: {db_path}")
+    return sqlite3.connect(str(db_path))
